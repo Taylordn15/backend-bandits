a4968c7fed1b6d26dbfd5a36fc393fa833afba0a
James Walden <78229982+jwalden2019@users.noreply.github.com>
2021-05-04 10:46:29 -0400

    jwalden

---
+++ Reservations/reservations.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+
+const getRestaurants = async () => {
+	// -- Fetch Restaurant Data -- //
+	const getRestaurantData = await fetch(
+		"https://api.yelp.com/v3/businesses/search?term=food&location=Atlanta",
+		{
+			method: "GET",
+			headers: {
+				Authorization:
+					"Bearer KBMAS_tznmWcmtlKTyItZ4WLCNLdqZZUAiQLZSxQqsvMXe4bIKS_sqWowkrwOsiyGiJAYOuZecNNosfcsEcthEGVfhJ8au2S0NNY5fjnzXmotccTfaxZTDTIXT2QYHYx",
+			},
+		}
+	);
+	const restaurantDataJson = await getRestaurantData.json();
+	console.log(restaurantDataJson);
+};
+console.log(getRestaurants());
