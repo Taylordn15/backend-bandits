0a8e25d955818a1b05d8654bf6d02eb9ea00614a
Randi Reyna <77865204+randi09@users.noreply.github.com>
2021-05-05 14:15:57 -0500

    updated PORT

--- server/newindex.js
+++ server/newindex.js
@@ -1,7 +1,7 @@
 // -- Get Tools && Set PORT -- //
 
 if (process.env.NODE_ENV !== "production") {
-	require("dotenv").config();
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -16,18 +16,18 @@ const es6Renderer = require("express-es6-template-engine");
 const initializedPassport = require("./passport-config");
 const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(
-	"https://qaozkethcihyhwxqwyyb.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDA2NTU3NSwiZXhwIjoxOTM1NjQxNTc1fQ.HIBZbZyYMY7Jc1d4kxYVkESzL55qG7RqMPMzoWA1oFI"
+  "https://qaozkethcihyhwxqwyyb.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDA2NTU3NSwiZXhwIjoxOTM1NjQxNTc1fQ.HIBZbZyYMY7Jc1d4kxYVkESzL55qG7RqMPMzoWA1oFI"
 );
 initializedPassport(
-	passport,
-	(email) => users.find((user) => user.email === email),
-	(id) => users.find((user) => user.id === id)
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
 );
 
 // -- Set Port -- //
 
-const PORT = 5321;
+const PORT = 5322;
 
 // -- Middleware -- //
 
@@ -36,11 +36,11 @@ app.use(express.json());
 app.use(cors());
 app.use(flash());
 app.use(
-	session({
-		secret: process.env.SESSION_SECRET,
-		resave: false,
-		saveUninitialized: false,
-	})
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -53,17 +53,17 @@ app.set("view engine", "html");
 app.use(express.urlencoded({ extended: false }));
 
 function checkAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect("/login");
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
 function checkIfUserIsLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) {
-		return res.redirect("/");
-	}
-	next();
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
 }
 
 // -- Routes -- //
@@ -71,73 +71,73 @@ function checkIfUserIsLoggedIn(req, res, next) {
 // - Welcome && Login Page(s) - //
 
 app.get("/", checkAuthenticated, (req, res) => {
-	res.render("welcome", { locals: { name: req.user.name } });
+  res.render("welcome", { locals: { name: req.user.name } });
 });
 
 app.get("/login", checkIfUserIsLoggedIn, (req, res) => {
-	res.render("login");
+  res.render("login");
 });
 
 app.post(
-	"/login",
-	passport.authenticate("local", {
-		successRedirect: "/",
-		failureRedirect: "/login",
-		failureFlash: true,
-	})
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
 );
 
 // - Register Page - //
 
 app.get("/register", checkIfUserIsLoggedIn, (req, res) => {
-	res.render("register");
+  res.render("register");
 });
 
 app.post("/register", async (req, res) => {
-	const user = {
-		id: Date.now().toString(),
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password,
-	};
-	console.log(user);
-	const { data, error } = await supabase.from("User").insert(user);
-	console.log(data);
+  const user = {
+    id: Date.now().toString(),
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  console.log(user);
+  const { data, error } = await supabase.from("User").insert(user);
+  console.log(data);
 });
 
 app.post("/logout", (req, res) => {
-	req.logOut();
-	res.redirect("/login");
+  req.logOut();
+  res.redirect("/login");
 });
 
 // - Home Page - //
 
 app.get("/home", async (req, res) => {
-	res.render("home.html");
+  res.render("home.html");
 });
 
 // - Reservation(s) Page - //
 
 app.post("/reservation", async (req, res) => {
-	const { yelpinfo, error } = await supabase
-		.from("Restaurant")
-		.insert(yelpinfo);
-	console.log(data);
+  const { yelpinfo, error } = await supabase
+    .from("Restaurant")
+    .insert(yelpinfo);
+  console.log(data);
 });
 
 app.get("/reservation", async (req, res) => {
-	const { data, error } = await supabase.from("Restaurant").select();
-	console.log(data);
+  const { data, error } = await supabase.from("Restaurant").select();
+  console.log(data);
 });
 
 // - Profile Page - //
 
 app.get("/home", async (req, res) => {
-	res.render("profile");
+  res.render("profile");
 });
 
 // - Listening Post - //
 
 app.listen(PORT, () => {
-	console.log(`Your server is being hosted on localhost:${PORT}`);
+  console.log(`Your server is being hosted on localhost:${PORT}`);
 });
