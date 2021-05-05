e2df0907c9cc2cf18a36a8518be28b4fed113ed5
James Walden <78229982+jwalden2019@users.noreply.github.com>
2021-05-05 12:52:00 -0400

    jwalden

--- server/newindex.js
+++ server/newindex.js
@@ -1,3 +1,5 @@
+// -- Get Tools && Set PORT -- //
+
 if (process.env.NODE_ENV !== "production") {
 	require("dotenv").config();
 }
@@ -17,16 +19,18 @@ const supabase = createClient(
 	"https://qaozkethcihyhwxqwyyb.supabase.co",
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDA2NTU3NSwiZXhwIjoxOTM1NjQxNTc1fQ.HIBZbZyYMY7Jc1d4kxYVkESzL55qG7RqMPMzoWA1oFI"
 );
-
 initializedPassport(
 	passport,
 	(email) => users.find((user) => user.email === email),
 	(id) => users.find((user) => user.id === id)
 );
-//PORT
+
+// -- Set Port -- //
+
 const PORT = 5321;
 
-//middleware
+// -- Middleware -- //
+
 app.use(express.static("../public"));
 app.use(express.json());
 app.use(cors());
@@ -40,12 +44,12 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.engine("html", es6Renderer);
 app.set("views", "../views");
 app.set("view engine", "html");
 
-// access our form information inside of our req
+// -- Access Form -- //
+
 app.use(express.urlencoded({ extended: false }));
 
 function checkAuthenticated(req, res, next) {
@@ -62,12 +66,14 @@ function checkIfUserIsLoggedIn(req, res, next) {
 	next();
 }
 
-//welcome page
+// -- Routes -- //
+
+// - Welcome && Login Page(s) - //
+
 app.get("/", checkAuthenticated, (req, res) => {
 	res.render("welcome", { locals: { name: req.user.name } });
 });
 
-//login page
 app.get("/login", checkIfUserIsLoggedIn, (req, res) => {
 	res.render("login");
 });
@@ -81,7 +87,8 @@ app.post(
 	})
 );
 
-//register page
+// - Register Page - //
+
 app.get("/register", checkIfUserIsLoggedIn, (req, res) => {
 	res.render("register");
 });
@@ -97,47 +104,24 @@ app.post("/register", async (req, res) => {
 	const { data, error } = await supabase.from("User").insert(user);
 	console.log(data);
 });
-// app.post ("/register", async (req, res) => {
-//     try {
-//         const salt = await bcrypt.genSalt();
-//         const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-//         const user = {
-//             id: Date.now().toString(),
-//             name: req.body.name,
-//             email: req.body.email,
-//             password: hashedPassword,
-//         };
-//         //sending the user to our array "database"
-//         users.push(user);
-//         //User.Create(user)
-//         console.log(users);
-//         res.status(200).redirect("/login");
-//     } catch (error) {
-//         res.status(401).redirect("/register");
-//     }
-// });
 
 app.post("/logout", (req, res) => {
 	req.logOut();
 	res.redirect("/login");
 });
 
-//homepage
+// - Home Page - //
+
 app.get("/home", async (req, res) => {
 	res.render("home.html");
 });
 
-//reservations page
+// - Reservation(s) Page - //
+
 app.post("/reservation", async (req, res) => {
-	const { data, error } = await supabase.from("Reservation").insert([
-		{
-			Name: "Joey",
-			Date: "2021-02-04",
-			Restaurant: "Pappadeuxs",
-			Party_Size: 22,
-		},
-	]);
+	const { yelpinfo, error } = await supabase
+		.from("Restaurant")
+		.insert(yelpinfo);
 	console.log(data);
 });
 
@@ -146,10 +130,13 @@ app.get("/reservation", async (req, res) => {
 	console.log(data);
 });
 
-//profile page
+// - Profile Page - //
+
 app.get("/home", async (req, res) => {
 	res.render("profile");
 });
+
+// - Listening Post - //
 
 app.listen(PORT, () => {
 	console.log(`Your server is being hosted on localhost:${PORT}`);
