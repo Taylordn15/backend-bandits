60f7067a87a92a13d0823aa437ae266f764c64c4
randy chong <randychong27@yahoo.com>
2021-05-05 09:40:02 -0500

    updated routes for register page

--- server/index.js
+++ server/index.js
@@ -87,26 +87,39 @@ app.get("/register", checkIfUserIsLoggedIn, (req, res) => {
     res.render("register");
 });
 
-app.post ("/register", async (req, res) => {
-    try {
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+app.post("/register", async (req, res) => {
+    const user = {
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    };
+    console.log(user)
+    const { data, error } = await supabase
+    .from("User")
+    .insert(user);
+    console.log(data);
+  });
+// app.post ("/register", async (req, res) => {
+//     try {
+//         const salt = await bcrypt.genSalt();
+//         const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        const user = {
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
-        };
-        //sending the user to our array "database"
-        users.push(user);
-        //User.Create(user)
-        console.log(users);
-        res.status(200).redirect("/login");
-    } catch (error) {
-        res.status(401).redirect("/register");
-    }
-});
+//         const user = {
+//             id: Date.now().toString(),
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: hashedPassword,
+//         };
+//         //sending the user to our array "database"
+//         users.push(user);
+//         //User.Create(user)
+//         console.log(users);
+//         res.status(200).redirect("/login");
+//     } catch (error) {
+//         res.status(401).redirect("/register");
+//     }
+// });
 
 app.post("/logout", (req, res) => {
     req.logOut()
